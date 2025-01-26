cb7f076c0785c0aa996159418152e7f6fba120da
Dimi-crtt <di.chizhov04@mail.ru>
2025-01-26 23:07:16 +0200

    Add files via upload

---
+++ lab2_D.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+int RootCalculation()
+{
+	float N;
+	std::cout << "Enter the degree of the root" << std::endl;
+	std::cin >> N;
+	float X;
+	std::cout << "Enter a number" << std::endl;
+	std::cin >> X;
+
+	float res = pow(X, N);
+	std::cout << "Your answer: " << res << std::endl;
+
+	return 0;
+}
+
+int main()
+{
+	RootCalculation();
+
+	return 0;
+}

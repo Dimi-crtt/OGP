7f5b6ca64b28c0e423b09d92ee52739f41183914
Dimi-crtt <di.chizhov04@mail.ru>
2025-02-07 10:42:38 +0200

    Add files via upload

---
+++ lab3_D.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+    int x, i;
+    std::cout << "Enter your number and number of the bit u want to shift: " << std::endl;
+    std::cin >> x >> i;
+
+    if (x <= 0 || x >= 1000000000) {
+        std::cout << "Error" << std::endl;
+    }
+    if (i < 0 || i >= 32) {
+        std::cout << "Error" << std::endl;
+    }
+    
+    // shifting i bit with 0
+    //x = x & ( ~ (1 << i));
+
+    // shifting i bit with 1
+     x = x | (1 << i);
+    
+    std::cout << "Number X after shifting " << i << "-bit with 1: " << x << std::endl;
+
+    return 0;
+}
